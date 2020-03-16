154710ad4463afcb361af45b2609228c13fee2b5
Travis <travisnevins@outlook.com>
2020-03-15 22:11:34 -0400

    update all files

--- index.ts
+++ index.ts
@@ -1,78 +1,99 @@
 import fs = require("fs");
 import exp = require("express");
 import path = require("path");
+import { v4 as uuidv4 } from "uuid";
+/* 
+[
+  { "id": 0, "title": "Test Title", "text": "Test text" },
+  {
+    "id": 1,
+    "title": "Note Two",
+    "text": "This is a test please....hopefully it works"
+  },
+  {
+    "id": 2,
+    "title": "Demo Note",
+    "text": "Test this really really need thsi to work!"
+  }
+]
+
+*/
 
 class Note {
-  id: Number;
-  note: String;
-  constructor(id: Number, note: string) {
+  title: String;
+  text: string;
+  id: string;
+  constructor(id: string, title: string, text: string) {
+    this.title = title;
+    this.text = text;
     this.id = id;
-    this.note = note;
   }
 }
 
-// =============================================================
-const app = exp();
-const PORT = 8080;
-
-// Sets up the Express app to handle data parsing
-app.use(exp.urlencoded({ extended: true }));
-app.use(exp.json());
-app.use("/resources", exp.static(__dirname + "/assests"));
-
-let note1 = new Note(
-  0,
-  "this is a test to see if stuff works! some more randome stuff hopefully this is hidden by the rramework"
-);
-let note2 = new Note(1, "this is the second note. hopefully this work");
-let notes: Array<Note> = [];
-notes.push(note1);
-notes.push(note2);
-//---------------------------
-//    main page url
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
-//---------------------
-//apis
-app.get("/api/notes", function(req, res) {
-  //call get notes function from fs
-  res.json(notes);
-});
-app.get("/api/deletenote/:id", function(req, res) {});
-
-app.post("/api/note", function(req, res) {
-  const { id, content } = req.body;
-  let note = new Note(id, content);
-});
-
-app.get("*", function(req, res) {
-  res.status(404).send("Page Not found!");
-});
-
-app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
-});
-
-const writeDoc = async (data: string) => {
-  fs.writeFile("data.json", data, function(err) {
+const writeDoc = (data: string) => {
+  fs.writeFile("db.json", data, function(err) {
     if (err) {
       return console.error(err);
     }
     console.log("File created!");
   });
 };
-const readDoc = async () => {
-  try {
-    fs.readFile("data.json", (data: any) => {
-      //will need to make a list o objects that data gets called by front end
-      console.log(data);
-    });
-  } catch (err) {
-    console.log(`There was an error while trying to create the readme!`);
-    console.log(err);
-  }
-};
 
-const start = () => {};
-start();
+// ==================================================================
+const app = exp();
+const PORT = 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(exp.urlencoded({ extended: true }));
+app.use(exp.json());
+//app.use("/assets", exp.static(__dirname + "/public/assests"));
+//used this ^ on my own handwriten version
+app.use(exp.static("public"));
+
+let notes: Array<Note> = [];
+
+//----------------------------------
+//       main page url
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+app.get("/notes", function(req, res) {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+//---------------------
+//apis
+app.get("/api/notes", function(req, res) {
+  //call get notes function from fs
+  //await readDoc(); I know not the best thing to make it sync but its onload when this gets called and  i just needed to make sure this got run first
+  notes = JSON.parse(fs.readFileSync("db.json", "utf-8"));
+  res.json(notes);
+});
+app.delete("/api/notes/:id", function(req, res) {
+  let id = req.params.id;
+  console.log(id);
+  notes = notes.filter(el => el.id !== id);
+
+  console.log(notes);
+  let strnotes = JSON.stringify(notes);
+  writeDoc(strnotes);
+});
+
+app.post("/api/notes", function(req, res) {
+  const { title, text } = req.body;
+  let id = uuidv4(); //Math.floor(Math.random() * 200);
+  let note = new Note(id, title, text);
+  notes.push(note);
+  console.log(note);
+  console.log(notes);
+  let strnotes = JSON.stringify(notes);
+  writeDoc(strnotes);
+});
+
+//sets any other url to homepage
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
