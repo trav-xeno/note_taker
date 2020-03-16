22225d5d95c8cb6ec5b5da83d921a39bd3b6fdac
Travis <travisnevins@outlook.com>
2020-03-16 17:25:29 -0400

    forgot to update port for heroku

--- index.ts
+++ index.ts
@@ -41,7 +41,7 @@ const writeDoc = (data: string) => {
 
 // ==================================================================
 const app = exp();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
 app.use(exp.urlencoded({ extended: true }));
